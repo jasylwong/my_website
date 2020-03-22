43aaa7957aadc9a94b2d6a5968d2edc1d0c93f96
Jason <jasylwong@gmail.com>
2020-03-22 09:08:55 +0000

    add netlify parameters to contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,27 +1,53 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 function Contact() {
+  const [validated, setValidated] = useState(false);
+  
+  const handleSubmit = event => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true)
+  }
+
   return (
     <div>
       <br></br>
       <h1 class="titles">Fancy a chat?</h1>
+      <br></br>
       <div class="contact-form">
-        <Form>
+        <p>
+          So would I.
+          Fill out the form below, and I'll get back to you as soon as possible.
+        </p><br></br>
+        <Form noValidate validated={validated} onSubmit={handleSubmit} action="POST" data-netifly="true">
           <Form.Group controlId="formBasicName">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter name" />
+            <Form.Label>Name:</Form.Label>
+            <Form.Control type="text" placeholder="Enter name" required/>
+            <Form.Control.Feedback type="invalid">
+              Please tell me who you are, so I know who to reply to!
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Label>Email:</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" required/>
+            <Form.Control.Feedback type="invalid">
+              Please enter a valid email so I can reply to you!
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group controlId="formBasicMessage">
-            <Form.Label>Message</Form.Label>
-            <Form.Control type="text" placeholder="Enter message" />
+            <Form.Label>Message:</Form.Label>
+            <Form.Control as="textarea" rows="5" placeholder="Enter message" required/>
+            <Form.Control.Feedback type="invalid">
+              Please enter a message so I know what you want to chat about!
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Button variant="primary" type="submit">
