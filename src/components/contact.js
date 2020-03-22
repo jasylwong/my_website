ecca943e357da34147e065b03f5ca5b1cd622edc
Jason <jasylwong@gmail.com>
2020-03-22 11:38:45 +0000

    test netlify form

--- src/components/contact.js
+++ src/components/contact.js
@@ -55,6 +55,7 @@ function Contact() {
           </Button> */}
         {/* </Form> */}
         <form name="contact" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact" />
           <p>
             <label>Your Name: <input type="text" name="name" /></label>   
           </p>
