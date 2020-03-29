890fd0da0825b56975c5e4d2a6065056c5f6dde2
Jason <jasylwong@gmail.com>
2020-03-29 13:27:41 +0100

    use class instead of id

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -7,7 +7,7 @@ function LandingPage() {
   return (
     <div>
       <div>
-        <img id="landing-pic" src="https://images.pexels.com/photos/775907/pexels-photo-775907.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="desk" />
+        <img class="landing-pic" src="https://images.pexels.com/photos/775907/pexels-photo-775907.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="desk" />
       </div>
       <div>
         <img src="https://images.pexels.com/photos/775907/pexels-photo-775907.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="desk" />
