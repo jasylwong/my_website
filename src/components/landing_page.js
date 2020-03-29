cb277587b91b1255a8323c23c766f33a97858cc8
Jason <jasylwong@gmail.com>
2020-03-29 13:22:35 +0100

    test landing page with additional nested div

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -10,6 +10,7 @@ function LandingPage() {
         <img id="landing-pic" src="https://images.pexels.com/photos/775907/pexels-photo-775907.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="desk" />
       </div>
       <div class="polaroid">
+          <img src="https://images.pexels.com/photos/775907/pexels-photo-775907.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="desk" />
           <img src={Me} alt="me"/>
       </div>
     </div>
