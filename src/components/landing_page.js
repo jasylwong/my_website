8dedc2950cdb6e9e16d35c923565245a9cb0009f
Jason <jasylwong@gmail.com>
2020-03-29 13:48:28 +0100

    test landing pic

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -6,9 +6,6 @@ import Me from '../images/D50_3710.jpg'
 function LandingPage() {
   return (
     <div>
-      <div class="landing_pic">
-        <img class="landing_pic" src="https://images.pexels.com/photos/775907/pexels-photo-775907.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="desk" />
-      </div>
       <div>
         <img class="land" src="https://images.pexels.com/photos/775907/pexels-photo-775907.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="desk" />
       </div>
