fbf9bf71b3f6da43a5fd2757ce390b91d98d8e27
Jason <jasylwong@gmail.com>
2020-03-29 13:17:08 +0100

    test landing page with me photo

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -1,11 +1,15 @@
 import React from 'react';
 // import desk from '../images/desk.jpg';
 import japan from '../images/japan.JPG';
+import Me from '../images/D50_3710.jpg'
 
 function LandingPage() {
   return (
     <div>
       <img id="landing-pic" src="https://images.pexels.com/photos/775907/pexels-photo-775907.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="desk" />
+      <div class="polaroid">
+          <img src={Me} alt="me"/>
+      </div>
     </div>
   )
 }
