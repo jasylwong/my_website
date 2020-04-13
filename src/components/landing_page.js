3f8a7ca25d8c1cbd2f4ce2fc409c5708c25e487a
Jason <jasylwong@gmail.com>
2020-04-13 19:06:18 +0100

    remove unused landing page pic

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -8,7 +8,6 @@ function LandingPage() {
     <div>
       <div>
         <img class="landing-pic" src="https://images.pexels.com/photos/775907/pexels-photo-775907.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="desk" />
-        {/* <img class="landing-pic" src={japan} alt="desk" /> */}
         <div class="white-box">
           <br></br>
           <div class="white-box-name">Jason Wong</div><br></br>
