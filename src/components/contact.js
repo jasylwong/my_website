ef3a51eba6cb3d52295908f4224c6cc79d40a127
Jason <jasylwong@gmail.com>
2020-04-15 23:05:09 +0100

    add html form in contacts page

--- src/components/contact.js
+++ src/components/contact.js
@@ -18,7 +18,28 @@ class Contact extends React.Component {
   render() {
     return(
       <div>
-        
+        <br></br>
+        <h1 class="titles">Fancy a chat?</h1>
+        <div>
+          <Form>
+            <Form.Group>
+              <Form.Label htmlFor="full-name">Full name:</Form.Label>
+              <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+            </Form.Group>
+            
+            <Form.Group>
+              <Form.Label htmlFor="email">Email:</Form.Label>
+              <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label htmlFor="message">Message:</Form.Label>
+              <Form.Control id="message" name="message" as="textarea" value={this.state.message} onChange={this.handleChange} rows="3" />
+            </Form.Group>
+
+            <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled}>Send message</Button>
+          </Form>
+        </div>
       </div>
     )
   }
