c6e1e437b36d6a44fe39d345e09819aaebb036be
Jason <jasylwong@gmail.com>
2020-04-15 22:50:50 +0100

    restart contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,25 +1,16 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
-function Contact() {
-  return (
-    <div>
-      <br></br>
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>Your Name: <input type="text" name="name" /></label>   
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    </div>
-  )
+class Contact extends React.Component {
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
 }
 
 export default Contact;
