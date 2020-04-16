2bfce560539589aa0202693224df993e67f2f3d2
Jason <jasylwong@gmail.com>
2020-04-16 20:57:04 +0100

    correct body-parser syntax

--- my_website_api/app.js
+++ my_website_api/app.js
@@ -1,5 +1,20 @@
 const express = require('express');
-const express = require('express');
-const express = require('express');
-const express = require('express');
-const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const sendGrid = require('@sendGrid/mail');
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.get('/api', (req, res, next) => {
+  res.send('API Status: Running')
+})
