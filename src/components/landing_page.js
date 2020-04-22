64d2c8749f4f5afbd19f763266b97a1ef3126872
Jason <jasylwong@gmail.com>
2020-04-22 09:58:07 +0100

    add fade in for landing page name box

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -8,7 +8,7 @@ function LandingPage() {
       <div>
         {/* <img class="landing-pic" src="https://images.pexels.com/photos/775907/pexels-photo-775907.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="background" /> */}
         <img class="landing-pic" src={japan} alt="Japan" />
-        <div class="white-box">
+        <div class="white-box fade-in">
           <br></br>
           <div class="white-box-name">Jason Wong</div><br></br>
           <div class="white-box-job">Full stack software developer</div>
